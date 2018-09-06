201b3ced920626d4ee7989023eba7e55819119de
A Hjerpe <adam.hjerpe@gmail.com>
2018-09-06 13:57:34 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <RouterNav></RouterNav>
+    <img id="logo" alt="Vue logo" src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+  }
+  #logo {
+    grid-area: logo;
+  }
+</style>
+
+<script>
+import RouterNav from '@/components/RouterNav.vue'
+export default {
+  components: {
+    RouterNav
+  }
+}
+</script>
